df0f3c5a2fe532ea81fce78710f6d68e74bc2dcf
Ilie Neacsu <ilie.neacsu@protonmail.com>
2024-02-08 15:52:13 +0200

    Create User entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+}

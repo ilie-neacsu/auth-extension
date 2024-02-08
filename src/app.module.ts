187de68dd83ecd61cecd20a503502da979fe20b8
Ilie Neacsu <ilie.neacsu@protonmail.com>
2024-02-08 15:51:18 +0200

    Add TypeORM dependency and postgresql configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CooffeesModule } from './cooffees/cooffees.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IamModule } from './iam/iam.module';
 
 @Module({
-  imports: [CooffeesModule],
+  imports: [
+    CooffeesModule,
+    UsersModule, TypeOrmModule.forRoot(
+      {
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'pass123',
+        database: 'postgres',
+        autoLoadEntities: true,
+        synchronize: true,
+      }
+    ), IamModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

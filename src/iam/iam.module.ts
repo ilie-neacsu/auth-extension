487a13506469e9909433cc461e4d2f2e6d466876
Ilie Neacsu <ilie.neacsu@protonmail.com>
2024-02-11 03:24:15 +0200

    Add public routes

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -10,6 +10,7 @@ import jwtConfig from "./config/jwt.config";
 import { ConfigModule } from "@nestjs/config";
 import { APP_GUARD } from "@nestjs/core";
 import { AccessTokenGuard } from "./authentication/guards/access-token/access-token.guard";
+import { AuthenticationGuard } from "./authentication/guards/authentication/authentication.guard";
 
 @Module({
   imports: [
@@ -19,15 +20,16 @@ import { AccessTokenGuard } from "./authentication/guards/access-token/access-to
   ],
   providers: [
     {
-      provide: APP_GUARD,
-      useClass: AccessTokenGuard,
-    },
-    {
       provide: HashingService,
       useClass: BcryptService,
     },
-    AuthenticationService
+    {
+      provide: APP_GUARD,
+      useClass: AuthenticationGuard,
+    },
+    AccessTokenGuard,
+    AuthenticationService,
   ],
-  controllers: [AuthenticationController]
+  controllers: [AuthenticationController],
 })
 export class IamModule {}
